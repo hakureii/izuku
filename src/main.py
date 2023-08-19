7c36aeb32ee5aa03e961fef0d290d882c9762308
hakureii <hakurei@asia.com>
2023-08-19 12:22:55 +0530

    web ook

--- src/main.py
+++ src/main.py
@@ -88,4 +88,12 @@ async def on_message(message):
     await message.delete()
   await bot.process_commands(message)
 
+@bot.tree.command(name="emoji", description="send animated emotes without nitro!")
+async def emoji(ctx, name:str):
+  webhook = await ctx.channel.create_webhook(name=ctx.author.display_name)
+  await webhook.send(str(name), username=ctx.author.display_name, avatar_url=ctx.author.avatar_url)
+  webhooks = await ctx.channel.webhooks()
+  for webhook in webhooks:
+    await webhook.delete()
+
 bot.run(os.environ["TOKEN"])
